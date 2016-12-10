a07bbf17be2d559e843e163572e9c11c00627921
Clément Dumazy <clement.dumazy@gmail.com>
2016-12-10 21:27:35 +0100

    Ajout d'un include pour le menu + ajout du panier avec la page de personnalisation de la pizza

---
+++ menu.js
@@ -0,0 +1,44 @@
+document.write('\
+\
+<nav class="navbar navbar-default" role="navigation">\
+    <div class="container">\
+        <!-- Brand and toggle get grouped for better mobile display -->\
+        <div class="navbar-header">\
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">\
+                <span class="sr-only">Toggle navigation</span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+            </button>\
+            <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->\
+            <a class="navbar-brand" href="index.html">Rapide Pizza</a>\
+        </div>\
+        <!-- Collect the nav links, forms, and other content for toggling -->\
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+            <ul class="nav navbar-nav">\
+                <li>\
+                    <a href="index.html">Accueil</a>\
+                </li>\
+                <li class="dropdown">\
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">La carte <span class="caret"></span></a>\
+    <ul class="dropdown-menu">\
+    <li><a href="carte-menus.html">MENUS</a></li>\
+    <li><a href="carte-pizzas.html">PIZZAS</a></li>\
+    <li><a href="carte-entrees.html">ENTRÉES</a></li>\
+    <li><a href="carte-desserts.html">DESSERTS</a></li>\
+    <li><a href="carte-boissons.html">BOISSONS</a></li>\
+  </ul>\
+                </li>\
+                <li>\
+                    <a href="about.html">A propos de nous</a>\
+                </li>\
+                <li>\
+                    <a href="contact.html">Contact</a>\
+                </li>\
+            </ul>\
+        </div>\
+        <!-- /.navbar-collapse -->\
+    </div>\
+    <!-- /.container -->\
+</nav>\
+');
